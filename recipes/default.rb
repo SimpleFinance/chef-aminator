54d244b35b611e0fef452f698f396d02a5f42a82
Will Maier <wcmaier@m.aier.us>
2013-04-21 18:33:26 +0000

    Leetle fixes.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,22 @@
 include_recipe 'python'
 
+package 'git'
+
 user node[:aminator][:user]
+
+directory node[:aminator][:root] do
+  owner node[:aminator][:user]
+  recursive true
+end
 
 python_virtualenv node[:aminator][:root] do
   owner node[:aminator][:user]
+  action :create
 end
 
 python_pip node[:aminator][:package] do
   package_name 'aminator'
   virtualenv node[:aminator][:root]
+  action :install
 end
 
